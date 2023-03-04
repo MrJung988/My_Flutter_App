95c3515d941026228ba8ac9a5cc4e52ddb476f6d
MrJung988 <karkisamir99@gmail.com>
2023-03-04 18:08:47 +0545

    Home Page added

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,1 +1,23 @@
 import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.orange[700],
+      ),
+      body: Center(
+        child: Text('Hello home'),
+      ),
+    );
+  }
+}
