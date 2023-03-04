1a8a6bfeb66ef54c1c2f9feac66f01e36238ef9d
MrJung988 <karkisamir99@gmail.com>
2023-03-04 20:17:58 +0545

    Drawer added in home page

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -13,8 +13,89 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
-        automaticallyImplyLeading: false,
         backgroundColor: Colors.orange[700],
+      ),
+      drawer: new Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.orange[700],
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://tabib.ae/uploads/profile/free-profile-photo-whatsapp-4_1617693292_1625640135.png'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Full Name'),
+                        Row(
+                          children: [
+                            Icon(Icons.phone_android),
+                            Text('+977 9876543210')
+                          ],
+                        ),
+                        Row(
+                          children: [Icon(Icons.email), Text('abc@gmail.com')],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Contact'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text('Manage Favorite'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.question_mark),
+              title: Text('FAQ'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
       body: Center(
         child: Text('Hello home'),
@@ -52,7 +133,7 @@ class _HomeState extends State<Home> {
           ),
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.menu,
+              Icons.more,
               color: Colors.black,
             ),
             label: 'More',
